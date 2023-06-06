b5ed0f8c8b03687517d0f08ada08b3f922c15ab6
vince <mrblack913@naver.com>
2023-06-06 12:57:47 +0900

    init

---
+++ src/main/java/kr/co/mpnet/dealing/DealingApplication.java
@@ -0,0 +1,13 @@
+package kr.co.mpnet.dealing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DealingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/mpnet/dealing/DealingApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.mpnet.dealing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
