ed9bd3a594a589cac75340eaa690c776f1ed2490
vince <mrblack913@naver.com>
2023-06-06 22:46:32 +0900

    first pub

---
+++ src/main/java/kr/co/mpnet/dealing/controller/HomeController.java
@@ -0,0 +1,14 @@
+package kr.co.mpnet.dealing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping
+    public String index() {
+        return "index";
+
+    }
+}
